3ece5be43b972584741c5795d9a3c58a3974100b
Rafael Paulino <rafaclasses@gmail.com>
2013-01-05 17:00:04 -0200

    Vetores
    Estudos iOS

--- Vetores/PrincipalAppDelegate.m
+++ Vetores/PrincipalAppDelegate.m
@@ -13,6 +13,99 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    NSString *texto = [[NSString alloc] init];
+    
+    /*
+      [texto retain];
+      [texto release];
+      nao precisamos lidar com retain e release pq estamos usando o ARC
+      no momento que nos perdemos as referencias o objeto morre
+    */
+    
+    //vetores - array
+    NSArray *listaNomes;
+    
+    listaNomes = [[NSArray alloc] init];
+    
+    //NSarray e um vetor estatico ou seja depois que eu criei com alloc/init, nao consigo mais muda-lo
+    
+    NSArray *meusParentes = [[NSArray alloc] initWithObjects:@"Joao",@"Marlene",@"Rafael", nil];
+    //USANDO UM CONTRUTOR  DA CLASSE nsaRRAY E PASSANdo uma lista de NSStrings para serem aramazendaas
+    
+    //o nil no final da lista indica que ela acabou (sentinela)
+    
+    //como acessar as informacoes de um array
+    //description e uma string que representa o objeto print_r()
+    NSLog(@"Dericao do vetor: %@", meusParentes.description);
+    
+    //como acessar uma posicao especfica do vetor
+    NSString *posicaoAcessada = [meusParentes objectAtIndex:1];
+    
+    NSLog(@"Pessoa consultada: %@", posicaoAcessada);
+    
+    //NSMutableArray permite alteracoes apos sua criacao
+    NSMutableArray *listaAlunos = [[NSMutableArray alloc] init];
+    
+    //adicionando o objeto ao array
+    [listaAlunos addObject:@"Gustavo"];
+    [listaAlunos addObject:@"Rafael"];
+    [listaAlunos addObject:@"Viviane"];
+    
+    //adicionando o aluno
+    [listaAlunos insertObject:@"Eduardo" atIndex:1];
+    
+    NSLog(@"Lista alunos: %@", listaAlunos.description);
+    
+    //mude o item do array pelo index dele
+    [listaAlunos replaceObjectAtIndex:0 withObject:@"Fernando"];
+    
+    NSLog(@"Lista alunos: %@", listaAlunos.description);
+    
+    [listaAlunos removeObjectAtIndex:0]; //remove pelo indice
+    
+    NSLog(@"Lista alunos: %@", listaAlunos.description);
+    
+    NSMutableArray *listaNumeros = [[NSMutableArray alloc] init];
+    
+    NSNumber *numero1 = [NSNumber numberWithInt:20];
+    NSNumber *numero2 = [NSNumber numberWithInt:40];
+    NSNumber *numero3 = [NSNumber numberWithInt:60];
+    NSNumber *numero4 = [NSNumber numberWithInt:80];
+    
+    [listaNumeros addObject:numero1];
+    [listaNumeros addObject:numero2];
+    [listaNumeros addObject:numero3];
+    [listaNumeros addObject:numero4];
+    
+    NSLog(@"Lista numeros: %@", listaNumeros.description);
+    
+    
+    /*
+     NSMutableArray *listaNumeros2 = [[NSMutableArray alloc] init];
+    
+    NSNumber *n = [NSNumber numberWithInt:0];
+    NSNumber *n2 = [NSNumber numberWithInt:4];
+    
+    while (n<n2) {
+        
+        NSNumber *numero[n] = [NSNumber numberWithInt:n];
+        [listaNumeros2 addObject:numero[n]];
+        n++;
+        
+    }
+    NSLog(@"Lista numeros2: %@", listaNumeros2.description);
+    
+    while (n<n2) {
+        NSLog(@"Lista numeros: %@", listaNumeros.description);
+        n + 1;
+    }*/
+    
+    
+    
+    
+    
+    
     return YES;
 }
 							
